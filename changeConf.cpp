4e5617b78bc5082b73eb94c6ec9ab3a48a64777d
Jicquel Vincent <vincent.jicquel@gmail.com>
2016-11-09 21:51:05 +0100

    fonction ext

--- changeConf.cpp
+++ changeConf.cpp
@@ -11,49 +11,6 @@ int funcTst(void* nom)
 	return 5;
 }
 
-<<<<<<< HEAD
-int main(void) {
-  struct lxc_container *c;
-  int ret = 1;
-  char *argv[] = {"echo", "salut", NULL};
-    lxc_attach_command_t command = {"echo", argv};
-  lxc_attach_options_t options = LXC_ATTACH_OPTIONS_DEFAULT;
-  lxc_attach_exec_t salut;
-  pid_t pid;
-  /* Setup container struct */
-  c = lxc_container_new("apicontainer", NULL);
-  if (!c) {
-    fprintf(stderr, "Failed to setup lxc_container struct\n");
-    goto out;
-  }
-
-
-  /* Start the container */
-  if (!c->start(c, 0, NULL)) {
-    fprintf(stderr, "Failed to start the container\n");
-    goto out;
-  }
-
-  /* Query some information */
-  printf("Container state: %s\n", c->state(c));
-  printf("Container PID: %d\n", c->init_pid(c));
-
-  if(c->attach(c,lxc_attach_run_command,&command,&options,&pid)!=0){
-    printf("dommage ça marche pas");
-  }
-
-
-  /* Stop the container */
-  if (!c->shutdown(c, 30)) {
-    printf("Failed to cleanly shutdown the container, forcing.\n");
-    if (!c->stop(c)) {
-      fprintf(stderr, "Failed to kill the container.\n");
-      goto out;
-    }
-  }
-
-  ret = 0;
-=======
 int main(int argc, char ** argv) {
 	int ret = 1, tst;
 	pid_t pid;
@@ -105,7 +62,6 @@ int main(int argc, char ** argv) {
 	}
 
 	ret = 0;
->>>>>>> a25e3028c4670e2202b4c9c8e7ee395105043169
 out:
 	lxc_container_put(c);
 	return ret;
