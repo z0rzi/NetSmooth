96773913707c47a29cc8a336168451c62e8bb683
root <root@localhost.localdomain>
2016-11-19 15:44:31 +0100

    ajout commentaires + modif un peu Entitee

---
+++ src/123JeSuisDansLesBois/Constantes.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTES_DEF
+#define CONSTANTES_DEF
+
+/* fonction setEtatMachine() dans Entitee */
+#define MACHINE_LANCEE true
+#define MACHINE_STOPEE false
+
+/* variable Type dans Entitee */
+#define TYPE_MACHINE 0
+#define TYPE_PASSERELLE 1
+#define TYPE_HUB 2
+
+#endif
