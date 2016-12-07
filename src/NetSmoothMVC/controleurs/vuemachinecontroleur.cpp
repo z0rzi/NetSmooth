f893ad34ff96eabb69dfe4494075fe56bff97cd9
z0rzi <skydax77@gmail.com>
2016-12-07 22:59:04 +0100

    lancer Entitee dans background en meme temps que physique

--- src/NetSmoothMVC/controleurs/vuemachinecontroleur.cpp
+++ src/NetSmoothMVC/controleurs/vuemachinecontroleur.cpp
@@ -2,25 +2,27 @@
 
 VueMachineControleur::VueMachineControleur(VueMachine* vue0, VueEntiteeControleur *parent) : VueEntiteeControleur(parent)
 {
-    vue = vue0;
+	vue = vue0;
 
-    connect(vue->zoneImg,SIGNAL(doubleClick()),this,SLOT(allumer()));
+	connect(vue->zoneImg,SIGNAL(doubleClick()),this,SLOT(allumer()));
 }
 
 void VueMachineControleur::allumer()
 {
-    QPixmap* img = new QPixmap();
-    if (vue->etat == false)
-    {
-        vue->etat = true;
-        img->load("../build-NetSmoothMVC-Desktop-Debug/images/ordinateuron.png");
-    }
-    else
-    {
-        vue->etat = false;
-        img->load("../build-NetSmoothMVC-Desktop-Debug/images/ordinateuroff.png");
-    }
-    *img = img->scaled(130,100);
-    vue->zoneImg->setPixmap(*img);
+	QPixmap* img = new QPixmap();
+	if (vue->etat == false)
+	{
+		this->vue->etat = true;
+		this->vue->ordinateur->launchEntitee();
+		img->load("../build-NetSmoothMVC-Desktop-Debug/images/ordinateuron.png");
+	}
+	else
+	{
+		vue->etat = false;
+		this->vue->ordinateur->stopEntitee();
+		img->load("../build-NetSmoothMVC-Desktop-Debug/images/ordinateuroff.png");
+	}
+	*img = img->scaled(130,100);
+	vue->zoneImg->setPixmap(*img);
 }
 
