8b24def29bb95027f730efc18d38d6fc9f8011c3
Fran <fran@fran.fr>
2016-12-08 13:15:40 +0000

    deplacer entitee sur vue bug

--- src/NetSmoothMVC/vues/clicklabel.h
+++ src/NetSmoothMVC/vues/clicklabel.h
@@ -4,16 +4,25 @@
 #include <QObject>
 #include <QLabel>
 #include <QMouseEvent>
+#include <iostream>
+
+using namespace std;
 
 class ClickLabel : public QLabel
 {
     Q_OBJECT
 public:
     ClickLabel(QLabel *parent = 0);
-    void mouseDoubleClickEvent(QMouseEvent *e);
+    void mousePressEvent(QMouseEvent *e);
+    void mouseMoveEvent(QMouseEvent *e);
+    void mouseReleaseEvent(QMouseEvent *e);
+    static ClickLabel* getLabelEnSelection();
+    static void setLabelEnSelection(ClickLabel* label);
+
+private:
+    static ClickLabel* LabelEnSelection;
 
 signals:
-    void doubleClick();
 
 };
 
