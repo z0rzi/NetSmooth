cef912008a0fbe32d98c9a7e1a34f4a699b8af0d
Jicquel Vincent <vincent.jicquel@gmail.com>
2016-12-12 00:03:57 +0100

    Ca marche enfiiiiiiiiiiiin

--- src/NetSmoothMVC/vues/vuecable.cpp
+++ src/NetSmoothMVC/vues/vuecable.cpp
@@ -9,23 +9,114 @@ VueEntitee* VueCable::SecondeSelection = NULL;
 VueCable::VueCable(VueEntitee* v1, VueEntitee* v2,QWidget *parent)
         : QWidget(parent),m_v1(v1),m_v2(v2)
 {
-    this->cable = Cable::creerCable(v1->getModele(),v2->getModele());//à faire
+    this->cable = Cable::creerCable(v1->getModele(),v2->getModele());
 
     /**********TRANSPARENCE****************/
-    //this->setAttribute( Qt::WA_TranslucentBackground);
-    //this->setWindowFlags (Qt::FramelessWindowHint);
-    //this->setAutoFillBackground(false);
+    this->setAttribute( Qt::WA_TranslucentBackground);
+    this->setWindowFlags (Qt::FramelessWindowHint);
+    this->setAutoFillBackground(false);
     /**************************************/
 }
 
 void VueCable::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this);
-
     QPen myPen(Qt::black, 2, Qt::SolidLine);
     painter.setPen(myPen);
-    painter.drawLine(this->m_v1->pos(),this->m_v2->pos());
-    this->setGeometry(this->m_v1->pos().x(),this->m_v1->pos().y(),this->m_v2->pos().x()-this->m_v1->pos().x(),this->m_v2->pos().y()-this->m_v1->pos().y());
+    int x1,y1,  //représente les coordonnées dans le widget de la première vue
+                    x2,y2, //de la deuxième vue
+                    width,height,
+                    hautGaucheX, hautGaucheY;//Pour setGeometry (haut gauche du rectangle)
+
+
+    if(this->m_v1->pos().x() > this->m_v2->pos().x())
+    {
+        x1 =  this->m_v1->pos().x()-this->m_v2->pos().x();
+        x2 = 0;
+        width = x1;
+        hautGaucheX = this->m_v2->pos().x();
+
+        if(this->m_v1->pos().y() > this->m_v2->pos().y())
+        {
+            y1 = this->m_v1->pos().y()-this->m_v2->pos().y();
+            y2 = 0;
+            height = y1;
+            hautGaucheY = this->m_v2->pos().y();
+        }
+        else if(this->m_v1->pos().y() < this->m_v2->pos().y())
+        {
+            y1 = 0;
+            y2 = this->m_v2->pos().y()-this->m_v1->pos().y();
+            height = y2;
+            hautGaucheY = this->m_v1->pos().y();
+
+        }
+        else//m_v1y==m_v2y
+        {
+            y1=0;
+            y2=0;
+            height = 5; // epaisseur du trait
+            hautGaucheY = this->m_v2->pos().y();
+        }
+    }
+    else if(this->m_v1->pos().x() < this->m_v2->pos().x())
+    {
+        x1 = 0;
+        x2 = this->m_v2->pos().x()-this->m_v1->pos().x();
+        width = x2;
+        hautGaucheX = this->m_v1->pos().x();
+
+        if(this->m_v1->pos().y() > this->m_v2->pos().y())
+        {
+            y1 = this->m_v1->pos().y()-this->m_v2->pos().y();
+            y2 = 0;
+            height = y1;
+            hautGaucheY = this->m_v2->pos().y();
+        }
+        else if(this->m_v1->pos().y() < this->m_v2->pos().y())
+        {
+            y1 = 0;
+            y2 = this->m_v2->pos().y()-this->m_v1->pos().y();
+            height = y2;
+            hautGaucheY = this->m_v1->pos().y();
+        }
+        else
+        {
+            y1=0;
+            y2=0;
+            height = 5; // epaisseur du trait
+            hautGaucheY = this->m_v1->pos().y();
+        }
+    }
+    else
+    {
+        x1=0;
+        x2=0;
+        width = 5; //epaisseur du trait
+        hautGaucheX=this->m_v1->pos().x();
+
+        if(this->m_v1->pos().y() > this->m_v2->pos().y())
+        {
+            y1 = this->m_v1->pos().y()-this->m_v2->pos().y();
+            y2 = 0;
+            height = y1;
+            hautGaucheY = this -> m_v2->pos().y();
+        }
+        else if(this->m_v1->pos().y() < this->m_v2->pos().y())
+        {
+            y1 = 0;
+            y2 = this->m_v2->pos().y()-this->m_v1->pos().y();
+            height = y2;
+            hautGaucheY = this -> m_v1->pos().y();
+        }
+        else //si les deux vues sont confondues
+        {
+            return;
+        }
+    }
+
+    painter.drawLine(x1,y1,x2,y2);
+    this->setGeometry(hautGaucheX,hautGaucheY,width,height);
 }
 
 Cable* VueCable::getModele()
