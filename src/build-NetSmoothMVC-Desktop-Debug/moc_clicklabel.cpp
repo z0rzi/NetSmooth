8526c118bd5173466a71da963622584dc4a23ad5
Jicquel Vincent <vincent.jicquel@gmail.com>
2016-12-11 15:38:31 +0100

    vues transformées en label qui fonctionne

--- src/build-NetSmoothMVC-Desktop-Debug/moc_clicklabel.cpp
+++ src/build-NetSmoothMVC-Desktop-Debug/moc_clicklabel.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_ClickLabel_t {
-    QByteArrayData data[1];
-    char stringdata0[11];
+    QByteArrayData data[4];
+    char stringdata0[35];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -29,10 +29,13 @@ struct qt_meta_stringdata_ClickLabel_t {
     )
 static const qt_meta_stringdata_ClickLabel_t qt_meta_stringdata_ClickLabel = {
     {
-QT_MOC_LITERAL(0, 0, 10) // "ClickLabel"
+QT_MOC_LITERAL(0, 0, 10), // "ClickLabel"
+QT_MOC_LITERAL(1, 11, 10), // "rightClick"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 11) // "doubleClick"
 
     },
-    "ClickLabel"
+    "ClickLabel\0rightClick\0\0doubleClick"
 };
 #undef QT_MOC_LITERAL
 
@@ -42,21 +45,50 @@ static const uint qt_meta_data_ClickLabel[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x06 /* Public */,
+       3,    0,   25,    2, 0x06 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
 
 void ClickLabel::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        ClickLabel *_t = static_cast<ClickLabel *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->rightClick(); break;
+        case 1: _t->doubleClick(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (ClickLabel::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&ClickLabel::rightClick)) {
+                *result = 0;
+            }
+        }
+        {
+            typedef void (ClickLabel::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&ClickLabel::doubleClick)) {
+                *result = 1;
+            }
+        }
+    }
     Q_UNUSED(_a);
 }
 
@@ -84,6 +116,27 @@ int ClickLabel::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     _id = QLabel::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
     return _id;
+}
+
+// SIGNAL 0
+void ClickLabel::rightClick()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, Q_NULLPTR);
+}
+
+// SIGNAL 1
+void ClickLabel::doubleClick()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, Q_NULLPTR);
 }
 QT_END_MOC_NAMESPACE
